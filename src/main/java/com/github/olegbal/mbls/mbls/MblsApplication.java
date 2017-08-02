700444cfb5ed5179c4eed431437e2a6ad04674f3
aleh.balakhanau <aleh.balakhanau@instinctools.ru>
2017-08-02 12:15:31 +0300

    initial project structure

---
+++ src/main/java/com/github/olegbal/mbls/mbls/MblsApplication.java
@@ -0,0 +1,12 @@
+package com.github.olegbal.mbls.mbls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MblsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MblsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/olegbal/mbls/mbls/MblsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.olegbal.mbls.mbls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MblsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
