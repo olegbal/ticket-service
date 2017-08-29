c5f499bcc80e6b336902f9307d679bd90c4dc12b
olegbal <olegbalah@gmail.com>
2017-08-29 21:00:31 +0300

    Change project name

---
+++ src/main/java/com/github/olegbal/ticketservice/TicketServiceBackendApplication.java
@@ -0,0 +1,12 @@
+package com.github.olegbal.ticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceBackendApplication.class, args);
+    }
+}
--- src/test/java/com/github/olegbal/ticketservice/TicketServiceBackendApplicationTests.java
+++ src/test/java/com/github/olegbal/ticketservice/TicketServiceBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.olegbal.mbls.mbls;
+package com.github.olegbal.ticketservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MblsApplicationTests {
+public class TicketServiceBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
