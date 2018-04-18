22c3b1783c82fc138e13e3f0d834521333594afb
olegbal <olegbalah@gmail.com>
2018-04-19 01:08:05 +0300

    #15 - Add dto validations on backend

--- ticket-service-backend/src/main/java/com/github/olegbal/ticketservice/data/auth/LoginAndPasswordDto.java
+++ ticket-service-backend/src/main/java/com/github/olegbal/ticketservice/data/auth/LoginAndPasswordDto.java
@@ -5,14 +5,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginAndPasswordDto {
 
     @NotEmpty
+    @Size(min = 4, max = 15)
     private String login;
 
     @NotEmpty
+    @Size(min = 6, max = 15)
     private String password;
 }
--- ticket-service-backend/src/main/java/com/github/olegbal/ticketservice/data/auth/RegistrationDto.java
+++ ticket-service-backend/src/main/java/com/github/olegbal/ticketservice/data/auth/RegistrationDto.java
@@ -5,18 +5,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegistrationDto {
 
     @NotEmpty
+    @Size(min = 4, max = 15)
     private String login;
 
     @NotEmpty
+    @Size(min = 6, max = 15)
     private String password;
 
     @NotEmpty
+    @Size(min = 6, max = 254)
     private String email;
 
     private String firstName;
