5f61b3c8ef89b2055eff9601fe6c89dc3127e5b1
olegbal <olegbalah@gmail.com>
2018-04-30 01:40:10 +0300

    #19 - Update frontend Event model. Create EventPlace.

--- ticket-service-frontend/angular/src/app/data/Event.ts
+++ ticket-service-frontend/angular/src/app/data/Event.ts
@@ -1,11 +1,11 @@
+import { EventPlace } from "./EventPlace";
+
 export class Event {
   public constructor(public id: number,
                      public title: string,
                      public date: Date,
-                     public minPrice: number,
-                     public maxPrice: number,
                      public imgUrl: string,
-                     public address: string) {
+                     public eventPlace: EventPlace) {
 
   }
 
---
+++ ticket-service-frontend/angular/src/app/data/EventPlace.ts
@@ -0,0 +1,6 @@
+export class EventPlace {
+  constructor(public id: number,
+              public placeName: string,
+              public placeAddress: string) {
+  }
+}
